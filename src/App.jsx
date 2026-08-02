260b701664f47674fd4a07928a6985ca61b2a859
1337-dracula <absurdvox@gmail.com>
2026-08-02 20:35:45 +0000

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useState, useEffect } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Auth from './pages/Auth.jsx'
+import Checkout from './pages/Checkout.jsx'
+import Navbar from './components/Navbar.jsx'
 
 export default function App() {
 
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        const data = await response.json();
-        console.log(data);
-
-      } catch (err) {
-
-      }
-    }
-
-    fetchUsers()
-  }, [])
-  const [count, setCount] = useState(0);
-  return <button onClick={() => setCount(count + 1)}>{count}</button>
-
+  return <div className='app'>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  </div>;
 }
 
