b5f691a86aeecd9ad4156a9546204488e166270c
1337-dracula <absurdvox@gmail.com>
2026-08-03 21:11:51 +0000

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import Home from './pages/Home.jsx'
 import Auth from './pages/Auth.jsx'
 import Checkout from './pages/Checkout.jsx'
 import Navbar from './components/Navbar.jsx'
+import AuthProvider from './context/AuthContext.jsx'
 
 export default function App() {
 
-  return <div className='app'>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/checkout" element={<Checkout />} />
-    </Routes>
-  </div>;
+  return (
+    <AuthProvider>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
 }
 
